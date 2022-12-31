2b9e21f1af3e66a79a7354a64f2b83e0478041a9
Serene Wizard <17552589+SereneWizard@users.noreply.github.com>
2022-12-31 14:20:04 -0800

    changes to room

--- StudyBuddyReplicate/settings.py
+++ StudyBuddyReplicate/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'StudyBuddyReplicate.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ltbfgrnv',
-        'HOST': 'mahmud.db.elephantsql.com',
-        'USER': 'ltbfgrnv',
-        'PASSWORD': 'CjI7xVt6cmPPdEoxwCX_Ha-yjaCSR-ns',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'ltbfgrnv',
+        # 'HOST': 'mahmud.db.elephantsql.com',
+        # 'USER': 'ltbfgrnv',
+        # 'PASSWORD': 'CjI7xVt6cmPPdEoxwCX_Ha-yjaCSR-ns',
+        # 'PORT': '5432',
     }
 }
 
